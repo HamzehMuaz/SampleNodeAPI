3258c7aa308f3a0f260502e95ae5d60c224605fd
Hamzeh Muaz <hamzehmuaz@gmail.com>
2020-07-06 17:27:58 +0300

    added swagger

--- server.js
+++ server.js
@@ -2,16 +2,19 @@
 Promise = require('bluebird');
 
 const mongoose = require('./src/config/mongoose');
-const server = require('./src/config/hapi')();
 
 const { host, port } = require('./src/config/vars');
 
 mongoose.connect();
 
-server.start().then(() => {
-  console.log(`Server now running on Host: ${host} and Port: ${port}`);
-}).catch((error) => {
-  console.error('There was a problem with start the server', error);
-});
+const start = async () => {
+  // eslint-disable-next-line global-require
+  const server = await require('./src/config/hapi')();
+  server.start().then(() => {
+    console.log(`Server now running on Host: ${host} and Port: ${port}`);
+  }).catch((error) => {
+    console.error('There was a problem with start the server', error);
+  });
+};
 
-module.exports = server;
+start();
--- src/config/hapi.js
+++ src/config/hapi.js
@@ -1,4 +1,7 @@
 const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
 
 const routes = require('../api/routes');
 
@@ -6,18 +9,36 @@ const {
   host, port,
 } = require('./vars');
 
-module.exports = () => {
+module.exports = async () => {
   const server = Hapi.Server({
     host,
     port,
   });
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: {
+        info: {
+          title: 'Sample Node API',
+          version: '1.0',
+        },
+      },
+    },
+  ]);
 
   server.route(routes);
 
   server.route({
     path: '/',
     method: 'GET',
-    handler: (request, h) => 'V1.0',
+    options: {
+      description: 'Get Version',
+      notes: 'Returns current API version',
+      tags: ['api'],
+      handler: (request, h) => 'Version 1.0',
+    },
   });
 
   return server;
